72b341b32ab0b026d7210e7e0c7b30bd29592bc0
Nomahlubi <zamahlubi27koyana@gmail.com>
2021-06-14 11:46:03 +0200

    card widget

---
+++ lib/ui/widget/cards.dart
@@ -0,0 +1,73 @@
+import 'package:Live_Connected_Admin/core/model/user_community_count_dto.dart';
+import 'package:Live_Connected_Admin/ui/shared/app_colors.dart';
+import 'package:Live_Connected_Admin/ui/shared/text_styles.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Cards extends StatelessWidget {
+  final Image image;
+  var count;
+  var title;
+  Cards(
+      {Key key,
+      @required this.userCommunityCountDto,
+      this.count,
+      this.image,
+      this.title})
+      : super(key: key);
+
+  final UserCommunityCountDto userCommunityCountDto;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        margin: EdgeInsets.all(12),
+        child: Card(
+          elevation: 5,
+          child: ClipPath(
+            child: Container(
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    alignment: Alignment.topLeft,
+                    child: count,
+                  ),
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: Container(
+                      alignment: Alignment.center,
+                      height: 90,
+                      width: 120,
+                      decoration: BoxDecoration(
+                        color: primaryColorLight,
+                        borderRadius: BorderRadius.all(Radius.circular(25)),
+                      ),
+                      child: image,
+                    ),
+                  ),
+                  Container(
+                    alignment: Alignment.bottomLeft,
+                    child: title,
+                  ),
+                ],
+              ),
+              height: 250,
+              padding: EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                  border: Border(
+                      bottom: BorderSide(color: primaryColorDark, width: 5))),
+            ),
+            clipper: ShapeBorderClipper(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5)),
+            ),
+          ),
+        ),
+        decoration: new BoxDecoration(boxShadow: [
+          new BoxShadow(
+            color: primaryColorLight,
+            blurRadius: 20.0,
+          )
+        ]));
+  }
+}
--- lib/ui/widget/home_content.dart
+++ lib/ui/widget/home_content.dart
@@ -6,6 +6,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:step_progress_indicator/step_progress_indicator.dart';
 
+import 'cards.dart';
+
 class HomeContent extends StatelessWidget {
   final UserCommunityCountDto userCommunityCountDto;
 
@@ -24,67 +26,24 @@ class HomeContent extends StatelessWidget {
                   onTap: () {
                     Navigator.pushNamed(context, "EventView");
                   },
-                  child: Container(
-                      margin: EdgeInsets.all(12),
-                      child: Card(
-                        elevation: 5,
-                        child: ClipPath(
-                          child: Container(
-                            child: Column(
-                              children: <Widget>[
-                                Container(
-                                  alignment: Alignment.topLeft,
-                                  child: Text(
-                                    '${userCommunityCountDto.totalEvents}',
-                                    style: textStylePrimaryColorDarkLargeBold,
-                                  ),
-                                ),
-                                Align(
-                                  alignment: Alignment.topRight,
-                                  child: Container(
-                                    alignment: Alignment.center,
-                                    height: 90,
-                                    width: 120,
-                                    decoration: BoxDecoration(
-                                      color: primaryColorLight,
-                                      borderRadius:
-                                          BorderRadius.all(Radius.circular(25)),
-                                    ),
-                                    child: Image.asset(
-                                      'assets/icons/calendar.png',
-                                      height: 80,
-                                      width: 80,
-                                      fit: BoxFit.cover,
-                                      color: textColorLightBlue,
-                                    ),
-                                  ),
-                                ),
-                                Container(
-                                  alignment: Alignment.bottomLeft,
-                                  child: Text('Events',
-                                      style: textStyleBlueGreySmaller),
-                                ),
-                              ],
-                            ),
-                            height: 250,
-                            padding: EdgeInsets.all(20),
-                            decoration: BoxDecoration(
-                                border: Border(
-                                    bottom: BorderSide(
-                                        color: primaryColorDark, width: 5))),
-                          ),
-                          clipper: ShapeBorderClipper(
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(5)),
-                          ),
-                        ),
-                      ),
-                      decoration: new BoxDecoration(boxShadow: [
-                        new BoxShadow(
-                          color: primaryColorLight,
-                          blurRadius: 20.0,
-                        )
-                      ])),
+                  child: Cards(
+                    userCommunityCountDto: userCommunityCountDto,
+                    image: Image.asset(
+                      'assets/icons/calendar.png',
+                      height: 80,
+                      width: 80,
+                      fit: BoxFit.cover,
+                      color: textColorLightBlue,
+                    ),
+                    count: Text(
+                      '${userCommunityCountDto.totalEvents}',
+                      style: textStylePrimaryColorDarkLargeBold,
+                    ),
+                    title: Text(
+                      'Events',
+                      style: textStyleBlueGreySmaller,
+                    ),
+                  ),
                 ),
               ),
               UIHelper.horizontalSpaceSmall(),
@@ -93,67 +52,24 @@ class HomeContent extends StatelessWidget {
                   onTap: () {
                     Navigator.pushNamed(context, "usersView");
                   },
-                  child: Container(
-                      margin: EdgeInsets.all(12),
-                      child: Card(
-                        elevation: 5,
-                        child: ClipPath(
-                          child: Container(
-                            child: Column(
-                              children: <Widget>[
-                                Container(
-                                  alignment: Alignment.topLeft,
-                                  child: Text(
-                                    '${userCommunityCountDto.totalUsers}',
-                                    style: textStylePrimaryColorDarkLargeBold,
-                                  ),
-                                ),
-                                Align(
-                                  alignment: Alignment.topRight,
-                                  child: Container(
-                                    alignment: Alignment.center,
-                                    height: 90,
-                                    width: 120,
-                                    decoration: BoxDecoration(
-                                      color: primaryColorLight,
-                                      borderRadius:
-                                          BorderRadius.all(Radius.circular(25)),
-                                    ),
-                                    child: Image.asset(
-                                      'assets/icons/group.png',
-                                      height: 80,
-                                      width: 80,
-                                      fit: BoxFit.cover,
-                                      color: textColorLightBlue,
-                                    ),
-                                  ),
-                                ),
-                                Container(
-                                  alignment: Alignment.bottomLeft,
-                                  child: Text('Users',
-                                      style: textStyleBlueGreySmaller),
-                                ),
-                              ],
-                            ),
-                            height: 250,
-                            padding: EdgeInsets.all(20),
-                            decoration: BoxDecoration(
-                                border: Border(
-                                    bottom: BorderSide(
-                                        color: primaryColorDark, width: 5))),
-                          ),
-                          clipper: ShapeBorderClipper(
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(5)),
-                          ),
-                        ),
-                      ),
-                      decoration: new BoxDecoration(boxShadow: [
-                        new BoxShadow(
-                          color: primaryColorLight,
-                          blurRadius: 20.0,
-                        )
-                      ])),
+                  child: Cards(
+                    userCommunityCountDto: userCommunityCountDto,
+                    image: Image.asset(
+                      'assets/icons/group.png',
+                      height: 80,
+                      width: 80,
+                      fit: BoxFit.cover,
+                      color: textColorLightBlue,
+                    ),
+                    count: Text(
+                      '${userCommunityCountDto.totalUsers}',
+                      style: textStylePrimaryColorDarkLargeBold,
+                    ),
+                    title: Text(
+                      'Users',
+                      style: textStyleBlueGreySmaller,
+                    ),
+                  ),
                 ),
               ),
             ],
@@ -165,67 +81,24 @@ class HomeContent extends StatelessWidget {
                   onTap: () {
                     Navigator.pushNamed(context, "subscribersView");
                   },
-                  child: Container(
-                      margin: EdgeInsets.all(12),
-                      child: Card(
-                        elevation: 5,
-                        child: ClipPath(
-                          child: Container(
-                            child: Column(
-                              children: <Widget>[
-                                Container(
-                                  alignment: Alignment.topLeft,
-                                  child: Text(
-                                    '${userCommunityCountDto.totalSubscribers}',
-                                    style: textStylePrimaryColorDarkLargeBold,
-                                  ),
-                                ),
-                                Align(
-                                  alignment: Alignment.topRight,
-                                  child: Container(
-                                    alignment: Alignment.center,
-                                    height: 90,
-                                    width: 120,
-                                    decoration: BoxDecoration(
-                                      color: primaryColorLight,
-                                      borderRadius:
-                                          BorderRadius.all(Radius.circular(25)),
-                                    ),
-                                    child: Image.asset(
-                                      'assets/images/renew.png',
-                                      height: 80,
-                                      width: 80,
-                                      fit: BoxFit.cover,
-                                      color: textColorLightBlue,
-                                    ),
-                                  ),
-                                ),
-                                Container(
-                                  alignment: Alignment.bottomLeft,
-                                  child: Text('Subscriptions',
-                                      style: textStyleBlueGreySmaller),
-                                ),
-                              ],
-                            ),
-                            height: 250,
-                            padding: EdgeInsets.all(20),
-                            decoration: BoxDecoration(
-                                border: Border(
-                                    bottom: BorderSide(
-                                        color: primaryColorDark, width: 5))),
-                          ),
-                          clipper: ShapeBorderClipper(
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(5)),
-                          ),
-                        ),
-                      ),
-                      decoration: new BoxDecoration(boxShadow: [
-                        new BoxShadow(
-                          color: primaryColorLight,
-                          blurRadius: 20.0,
-                        )
-                      ])),
+                  child: Cards(
+                    userCommunityCountDto: userCommunityCountDto,
+                    image: Image.asset(
+                      'assets/images/renew.png',
+                      height: 80,
+                      width: 80,
+                      fit: BoxFit.cover,
+                      color: textColorLightBlue,
+                    ),
+                    count: Text(
+                      '${userCommunityCountDto.totalSubscribers}',
+                      style: textStylePrimaryColorDarkLargeBold,
+                    ),
+                    title: Text(
+                      'Subscriptions',
+                      style: textStyleBlueGreySmaller,
+                    ),
+                  ),
                 ),
               ),
               UIHelper.horizontalSpaceSmall(),
@@ -234,67 +107,24 @@ class HomeContent extends StatelessWidget {
                   onTap: () {
                     Navigator.pushNamed(context, "EventView");
                   },
-                  child: Container(
-                      margin: EdgeInsets.all(12),
-                      child: Card(
-                        elevation: 5,
-                        child: ClipPath(
-                          child: Container(
-                            child: Column(
-                              children: <Widget>[
-                                Container(
-                                  alignment: Alignment.topLeft,
-                                  child: Text(
-                                    '${userCommunityCountDto.totalChildren}',
-                                    style: textStylePrimaryColorDarkLargeBold,
-                                  ),
-                                ),
-                                Align(
-                                  alignment: Alignment.topRight,
-                                  child: Container(
-                                    alignment: Alignment.center,
-                                    height: 90,
-                                    width: 120,
-                                    decoration: BoxDecoration(
-                                      color: primaryColorLight,
-                                      borderRadius:
-                                          BorderRadius.all(Radius.circular(25)),
-                                    ),
-                                    child: Image.asset(
-                                      'assets/icons/boy.png',
-                                      height: 80,
-                                      width: 80,
-                                      fit: BoxFit.cover,
-                                      color: textColorLightBlue,
-                                    ),
-                                  ),
-                                ),
-                                Container(
-                                  alignment: Alignment.bottomLeft,
-                                  child: Text('Children',
-                                      style: textStyleBlueGreySmaller),
-                                ),
-                              ],
-                            ),
-                            height: 250,
-                            padding: EdgeInsets.all(20),
-                            decoration: BoxDecoration(
-                                border: Border(
-                                    bottom: BorderSide(
-                                        color: primaryColorDark, width: 5))),
-                          ),
-                          clipper: ShapeBorderClipper(
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(5)),
-                          ),
-                        ),
-                      ),
-                      decoration: new BoxDecoration(boxShadow: [
-                        new BoxShadow(
-                          color: primaryColorLight,
-                          blurRadius: 20.0,
-                        )
-                      ])),
+                  child: Cards(
+                    userCommunityCountDto: userCommunityCountDto,
+                    image: Image.asset(
+                      'assets/icons/boy.png',
+                      height: 80,
+                      width: 80,
+                      fit: BoxFit.cover,
+                      color: textColorLightBlue,
+                    ),
+                    count: Text(
+                      '${userCommunityCountDto.totalChildren}',
+                      style: textStylePrimaryColorDarkLargeBold,
+                    ),
+                    title: Text(
+                      'Children',
+                      style: textStyleBlueGreySmaller,
+                    ),
+                  ),
                 ),
               )
             ],
